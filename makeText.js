dc27c087280f753722bb03e64cc4f1a66f01d6af
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-08-27 01:15:22 -0500

    completed

--- makeText.js
+++ makeText.js
@@ -1,2 +1,40 @@
-/** Command-line tool to generate Markov text. */
+const {MarkovMachine} = require('./markov')
+const axios = require('axios')
+const fs = require('fs')
 
+let type = process.argv[2]
+let path = process.argv[3]
+
+function makeRandomizedText(text) {
+  let mm = new MarkovMachine(text)
+  console.log(mm.makeText())
+}
+
+function getText(path) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      // handle possible error
+      console.error(`Error reading ${path}: ${err}`)
+      // kill the process and tell the shell it errored
+      process.exit(1)
+    }
+    // otherwise success
+    makeRandomizedText(data)
+  })
+}
+
+async function getURLText(url) {
+  try {
+    res = await axios.get(`${url}`)
+    makeRandomizedText(res.data)
+  } catch (err) {
+    console.log(`Error reading ${url}: ${err}`)
+    process.exit(1)
+  }
+}
+
+if (type === 'url') {
+  getURLText(path)
+} else {
+  getText(path)
+}
